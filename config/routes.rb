b0f3238dc86c65046bd512cbe8d7e49ca4d3e23e
Raga <ragawilliam570@gmail.com>
2022-12-28 18:01:26 +0300

    Modify routes to add staffs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :students
+  devise_for :staffs, path: 'staffs', controllers: {
+    sessions: 'staffs/sessions',
+    registrations: 'staffs/registrations',
+    # confirmations: 'staffs/confirmations',
+    # mailer: 'staffs/mailers',
+    # passwords: 'staffs/passwords'
+  }
+  devise_for :students, path: 'students', controllers: {
+    sessions: 'students/sessions',
+    registrations: 'students/registrations',
+    # confirmations: 'students/confirmations',
+    # mailer: 'students/mailers',
+    # passwords: 'students/passwords'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
