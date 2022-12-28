db104fbdf86f84762017280b6b550a88e239c2d5
Raga <ragawilliam570@gmail.com>
2022-12-28 18:03:57 +0300

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_142439) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_143704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "staffs", force: :cascade do |t|
+    t.string "name"
+    t.integer "national_id"
+    t.text "staff_registration_number"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_staffs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
