16cd111d618556a104ceb6e71a360744f9b448f7
Raga <ragawilliam570@gmail.com>
2022-12-29 20:32:38 +0300

    Add admin fields

--- db/migrate/20221229172855_create_admins.rb
+++ db/migrate/20221229172855_create_admins.rb
@@ -1,7 +1,10 @@
 class CreateAdmins < ActiveRecord::Migration[7.0]
   def change
     create_table :admins do |t|
-      
+      t.name :text
+      t.admin_id :integer
+      t.national_id :integer
+      t.staff_id :integer
 
       t.timestamps
     end
