7da106f0a3a46835cb0a23c4a24c70be7b2c0c10
Raga <ragawilliam570@gmail.com>
2022-12-29 17:47:11 +0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_143704) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_141835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.text "name"
+    t.text "code"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "staffs", force: :cascade do |t|
     t.string "name"
