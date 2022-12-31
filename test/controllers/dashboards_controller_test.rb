72fc872d78968fcbbb66b658df060e70f6ed02df
Raga <ragawilliam570@gmail.com>
2022-12-31 17:29:50 +0300

    Add dashboards

---
+++ test/controllers/dashboards_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  test "should get admin" do
+    get dashboards_admin_url
+    assert_response :success
+  end
+
+  test "should get staff" do
+    get dashboards_staff_url
+    assert_response :success
+  end
+
+  test "should get student" do
+    get dashboards_student_url
+    assert_response :success
+  end
+end
