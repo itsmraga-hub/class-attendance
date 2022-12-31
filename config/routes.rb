57b8a97df854d6e3b42d297b6781b3ccfcb15755
Raga <ragawilliam570@gmail.com>
2022-12-31 15:26:36 +0300

    Add routes paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,45 +2,61 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: {
     sessions: 'admins/sessions',
     registrations: 'admins/registrations',
-    confirmations: 'admins/confirmations',
-    mailer: 'admins/mailers',
-    passwords: 'admins/passwords'
+    # confirmations: 'admins/confirmations',
+    # mailer: 'admins/mailers',
+    # passwords: 'admins/passwords'
   }
+
+  devise_scope :staffs do
+    get 'staffs/sign_in'
+  end
+  devise_scope :admins do
+    get 'admins/sign_in'
+  end
+  devise_scope :students do
+    get 'students/sign_in'
+  end
 
   devise_for :staffs, path: 'staffs', controllers: {
     sessions: 'staffs/sessions',
     registrations: 'staffs/registrations',
-    confirmations: 'staffs/confirmations',
-    mailer: 'staffs/mailers',
-    passwords: 'staffs/passwords'
+    # confirmations: 'staffs/confirmations',
+    # mailer: 'staffs/mailers',
+    # passwords: 'staffs/passwords'
   }
   devise_for :students, path: 'students', controllers: {
     sessions: 'students/sessions',
     registrations: 'students/registrations',
-    confirmations: 'students/confirmations',
-    mailer: 'students/mailers',
-    passwords: 'students/passwords'
+    # confirmations: 'students/confirmations',
+    # mailer: 'students/mailers',
+    # passwords: 'students/passwords'
   }
+
+  # resources :courses
 
   authenticated :admin do
     # root 'courses#index'
-    # resources :departments
-    # resources :courses
+    resources :departments
+    resources :courses
     # root '', as: :authenticated_root
   end
 
   authenticated :student do
     # root '', as: :authenticated_root
+    resources :departments
+    resources :courses
     # root 'courses#index'
   end
 
   authenticated :staff do
     # root 'courses#index'
+    resources :departments
+    resources :courses
     # root '', as: :authenticated_root
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'courses#index'
+  # root 'courses#index'
 end
