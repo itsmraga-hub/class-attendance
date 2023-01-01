c30044efb680e2bb02f57d0052ed73d608b32d88
Raga <ragawilliam570@gmail.com>
2023-01-01 15:26:41 +0300

    add check user to controllers

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Students::RegistrationsController < Devise::RegistrationsController
   include Accessible
+  skip_before_action :check_user, except: [:new, :create]
   # skip_before_action :check_resource, except: [:new, :create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
