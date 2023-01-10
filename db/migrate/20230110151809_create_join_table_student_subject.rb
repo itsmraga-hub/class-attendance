eb8d8ca37c2d3c99c28f5246b27a43e812cb47aa
Raga <ragawilliam570@gmail.com>
2023-01-10 18:26:06 +0300

    Create Student-Absent join table

--- db/migrate/20230110151809_create_join_table_student_subject.rb
+++ db/migrate/20230110151809_create_join_table_student_subject.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableStudentSubject < ActiveRecord::Migration[7.0]
   def change
     create_join_table :students, :subjects do |t|
-      t.index [:student_id, :subject_id]
+      # t.index [:student_id, :subject_id]
       # t.index [:subject_id, :student_id]
     end
   end
