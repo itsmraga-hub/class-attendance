30627b15613d6615cf74b21b727234f0f020a24b
Raga <ragawilliam570@gmail.com>
2023-01-13 16:02:53 +0300

    Fix lintre issues

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -48,12 +48,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :image, :name, :phone_number, :staff_id, :admin_id, :national_id])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[attribute image name phone_number staff_id admin_id national_id])
   end
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :image, :email, :password, :password_confirmation, :current_password) }
-  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- app/controllers/staffs/registrations_controller.rb
+++ app/controllers/staffs/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Staffs::RegistrationsController < Devise::RegistrationsController
   include Accessible
-  skip_before_action :check_user, except: %i[new create]
+  # skip_before_action :check_user, except: %i[new create]
   # skip_before_action :check_resource, except: [:new, :create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
