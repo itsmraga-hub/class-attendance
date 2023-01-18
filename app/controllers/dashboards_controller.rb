9248729dadd04201392f7b06b1e745fef364f8ed
Raga <ragawilliam570@gmail.com>
2023-01-18 23:48:31 +0300

    Add to dash

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,14 @@
 class DashboardsController < ApplicationController
   def student_dashboard; end
 
-  def staff_dashboard; end
+  def staff_dashboard
+    @students = Student.all
+
+  end
 
   def admin_dashboard
     # @admin = current_admin
     @departments = Department.all.where(admin: current_admin)
+    @students = Student.search(params[:search])
   end
 end
